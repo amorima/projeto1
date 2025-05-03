054ef919ee93825b8c3fae7c7d8eb7416a8b8dbb
Gabriel Paiva <mr.sousapaiva@gmail.com>
2025-05-04 00:04:28 +0100

    Modal Interativo (não funcional)

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/* Modal Gestão de Voos */
+function openModal() {
+    document.getElementById("modal-adicionar").classList.remove("hidden");
+  }
+function closeModal() {
+document.getElementById("modal-adicionar").classList.add("hidden");
+}
