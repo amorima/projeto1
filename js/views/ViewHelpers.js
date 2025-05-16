e1aa7cee309faa957ee4fccc0d2c33812562a625
Antonio Amorim <work@antonioamorim.pt>
2025-05-16 23:54:20 +0100

    feat: Add ViewHelpers.js and include it in multiple admin HTML files load header
    remove: index deprecated
    html testes

--- js/views/ViewHelpers.js
+++ js/views/ViewHelpers.js
@@ -1,5 +1,51 @@
 // ViewHelpers.js – funções de UI para as Views
 
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "Main-Primary": "#1B9AAA", // cor primária
+        "Main-Secondary": "#126B76",
+        "brand-secondary": "#126B76", // cor secundária
+        "Button-Main": "#126B76", // cor secundária
+        "Background-Card-Bg-Gami": "#F7F7F7",
+        "Text-Subtitles": "#808080",
+        "Components-Limit-Color": "", //Gradiente
+      },
+      fontFamily: {
+        sans: [
+          "IBM Plex Sans",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        mono: [
+          "Space Mono",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          '"Liberation Mono"',
+          '"Courier New"',
+          "monospace",
+        ],
+      },
+    },
+  },
+};
+
 export function getFormData(formId) {
   const form = document.getElementById(formId);
   const data = {};
@@ -128,12 +174,12 @@ export function loadComponent(componentPath, elementId) {
 // Carregar header automaticamente se existir o placeholder
 document.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("header-placeholder")) {
-    loadComponent("html/_header.html", "header-placeholder");
+    loadComponent("../html/_header.html", "header-placeholder");
   }
   if (document.getElementById("footer-placeholder")) {
-    loadComponent("html/_footer.html", "footer-placeholder");
+    loadComponent("../html/_footer.html", "footer-placeholder");
   }
   if (document.getElementById("menu-placeholder")) {
-    loadComponent("html/_menu.html", "menu-placeholder");
+    loadComponent("../html/_menu.html", "menu-placeholder");
   }
 });
