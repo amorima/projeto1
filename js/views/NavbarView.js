7934a2dc5b2d089442b89135dd2679662e47be33
Gabriel Paiva <mr.sousapaiva@gmail.com>
2025-05-29 11:07:17 +0100

    feat: Move theme loading script to NavbarView for better organization

--- js/views/NavbarView.js
+++ js/views/NavbarView.js
@@ -70,3 +70,10 @@ function LogIn () {
         }
     })
 }
+
+try {
+const theme = localStorage.getItem("theme");
+if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+}
+} catch (e) {}
